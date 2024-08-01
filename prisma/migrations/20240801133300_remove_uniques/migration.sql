fc2538cbaa63f5827448aac8dadd040d9d5a6234
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2024-08-01 17:34:23 +0400

    fix schema

---
+++ prisma/migrations/20240801133300_remove_uniques/migration.sql
@@ -0,0 +1,14 @@
+-- DropIndex
+DROP INDEX "Certificates_aboutMeId_key";
+
+-- DropIndex
+DROP INDEX "Educations_aboutMeId_key";
+
+-- DropIndex
+DROP INDEX "Languages_aboutMeId_key";
+
+-- DropIndex
+DROP INDEX "Socials_profileId_key";
+
+-- DropIndex
+DROP INDEX "Works_aboutMeId_key";
